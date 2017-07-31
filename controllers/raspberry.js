b2e81dfc306ea54cd4898b9c179b50eda660414e
Mauricio <mauricio.cadavid@yuxiglobal.com>
2017-07-31 13:43:38 -0500

    updating service

--- controllers/raspberry.js
+++ controllers/raspberry.js
@@ -4,23 +4,23 @@ var Gpio = require('onoff').Gpio,
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://10.3.8.212:1883');
 
+client.subscribe('LED');
+
+client.on('message', function(topic, message) {
+  console.log(message.toString());
+
+  led.writeSync(1);
+
+  setTimeout(function() {
+      led.writeSync(0);  // Turn LED off.
+      led.unexport();    // Unexport GPIO and free resources
+  }, 5000);
+
+  res.status(200).json({'message': message.toString()});
+
+});
+
 exports.turnOnOffLed = function(req, res){
-	led = new Gpio(21, 'out');
-	client.subscribe('LED');
-
-	client.on('message', function(topic, message) {
-	  console.log(message.toString());
-
-	  led.writeSync(1);
-
-	  setTimeout(function() {
-	      led.writeSync(0);  // Turn LED off.
-	      led.unexport();    // Unexport GPIO and free resources
-	  }, 5000);
-
-	  res.status(200).json({'message': message.toString()});
-
-	});
 
 	console.log('Client started...');
 }
