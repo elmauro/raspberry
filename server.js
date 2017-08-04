2ad42de6ab614496ef7e58d04dc38497c762ce67
elmauro <elmauro@gmail.com>
2017-08-04 15:35:41 +0000

    updating server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var Gpio = require('onoff').Gpio,
-	button = new Gpio(19, 'in', 'both'),
-    led = new Gpio(21, 'out');
+  led = new Gpio(21, 'out'),
+  button = new Gpio(19, 'in', 'both');
 
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://10.3.8.231:1883');
@@ -12,17 +12,26 @@ client.on('message', function(topic, message) {
   _message =  message.toString();
 
   switch(_message){
-  	case 'turnOnLed': led.writeSync(1);
-  					break;
+        case 'turnOnLed': led.writeSync(1);
+                          break;
 
-  	case 'turnOffLed': led.writeSync(0);
-  					break; 
+        case 'turnOffLed': led.writeSync(0);
+                           break;
   }
 
 });
 
-button.watch(function(err, value) {
-  if(value == 1) {
+button.watch(function (err, value) {
+  if (err) {
+    throw err;
+  }
+
+  if(value == 1){
     client.publish('LED', 'turnOn');
-  }    
+  }
+});
+
+process.on('SIGINT', function () {
+  led.unexport();
+  button.unexport();
 });
